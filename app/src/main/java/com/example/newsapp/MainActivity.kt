615b6e39f79a9b0560fa016c6e87845ad2f6e306
Cassio Filho <Cassio.Filho@fit-tecnologia.org.br>
2021-03-12 10:02:59 -0300

    Create News Page

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun OpenUrl(view: View) {
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://seekingalpha.com/article/4413421-gsah-rumors-create-opportunity"))
+        startActivity(i)
     }
 }
